b24e368c67b5e53a3b11ec1d7033e00d1c208b5e
eaten <eaten@LAPTOP-5H3QD1H9>
2024-03-07 11:27:12 -0500

    Add project files.

---
+++ GenericAnimalClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace blud
+{
+    public class Animal < A >
+    {
+        public A data;
+        public Animal( A data )
+        {
+            this.data = data;
+            Console.WriteLine("Animal Info - " + this.data);
+        }
+
+        public A getAnimal()
+        {
+            return data;
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Animal < string > animalName = new Animal < string >("Capybara");
+            Animal<string> animalHabitat = new Animal<string>("Dense Forest");
+            Animal<bool> endangered = new Animal<bool>(false);
+            Animal<bool> extinct = new Animal<bool>(false);
+            Animal<int> lifeSpan = new Animal<int>(6);
+         
+
+        }
+    }
+}
